f13dd267b1b4df7fac3127cd1a90a9a728feca44
Al <devopsy@alpro.broadband>
2024-01-25 17:24:11 +0000

    comment docker stop script

--- stop_container.sh
+++ stop_container.sh
@@ -2,5 +2,5 @@
 set -e
 
 # Stop the running container (if any)
-sudo docker stop $(docker ps -a -q)
-sudo docker rm $(docker ps -a -q)
+#sudo docker stop $(docker ps -a -q)
+#sudo docker rm $(docker ps -a -q)
