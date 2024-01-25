7e4187bc53138e51d3840761987c5a848931cdbd
techwithalok <learntechwithalok@gmail.com>
2024-01-25 21:05:32 +0000

    enable stop container script

--- stop_container.sh
+++ stop_container.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
 
-echo "hello"
-
-#sudo su - root
+sudo su - root
 # Stop the running container (if any)
-#docker stop $(docker ps -a -q)
-#docker rm $(docker ps -a -q)
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
